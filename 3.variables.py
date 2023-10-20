79511617c9cc5d0ee27e74e2a98654adbfd12168
themanoftalent <akif.cfici@tuwien.ac.at>
2023-10-20 11:57:15 +0200

    Synced with remote

---
+++ 3.variables.py
@@ -0,0 +1,54 @@
+##################################################
+#### **************************************** ####
+#### !/usr/bin/python3                        ####
+#### -*- coding: utf-8 -*-                    ####
+#### @Time    : 2023/20/10 11:40              ####
+#### @Author  : themanoftalent                ####
+#### @Site:https://github.com/themanoftalent  ####
+#### @Project : python-app                    ####
+#### **************************************** ####
+##################################################
+
+# String variable: It stores text data inside double or single quotes.
+message = "Hello Python World!"
+print(message)  # Output: Hello Python World!
+message2 = 'Hello Python Crash Course World!'
+print(message2)  # Output: Hello Python Crash Course World!
+
+# Integer variable: It stores whole numbers.
+number = 2
+print(number)  
+
+# Floating-point variables: They store numbers with decimal points.
+doublenumber = 2.0
+longnumber = 2.0000000
+print(doublenumber)  
+print(longnumber) 
+
+# List variable: It stores a collection of items and is denoted by square brackets.
+fruits = ["apple", "banana", "cherry"]
+print(fruits)  # Output: ['apple', 'banana', 'cherry']
+
+# Tuple variable: It's similar to a list but denoted by parentheses. Tuples are immutable (cannot be changed).
+coordinates = (3, 4)
+print(coordinates)  
+
+# Dictionary variable: It stores key-value pairs and is denoted by curly braces.
+person = {"name": "John", "age": 30, "city": "New York"}
+print(person)  # Output: {'name': 'John', 'age': 30, 'city': 'New York'}
+
+# Boolean variable: It stores either True or False.
+is_student = True
+is_teacher = False
+print(is_student)
+print(is_teacher) 
+
+# None variable: It represents the absence of a value or a null value.
+empty_value = None
+print(empty_value)
+
+
+# Complex number variable: It represents a complex number in Python.
+complex_num = 3 + 4j
+print(complex_num)  
+
