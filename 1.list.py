b9ae9c0855e7a89ddf55cad93365dd87999b442b
@themanoftalent <cs.akifcifci@gmail.com>
2023-10-20 11:32:59 +0200

    Synced with remote

---
+++ 1.list.py
@@ -0,0 +1,42 @@
+#! /Python 3.6.5
+#This is a list in python
+#List is a collection of items in a particular order
+
+bicycles = ['trek','cannondale','redline','specialized']  #This is a list of bicycles
+print(bicycles) #This will print the list of bicycles
+print("\n") #This will print a new line
+print(bicycles[0]) #This will print the first element of the list
+print("\n") #This will print a new line
+print(bicycles[0].title()) #This will print the first element of the list
+print("\n") #This will print a new line
+print(bicycles[1]) #This will print the second element of the list
+print("\n") #This will print a new line
+print(bicycles[3]) #This will print the fourth element of the list
+print("\n") #This will print a new line
+#now lets add to the list
+bicycles.append('ducati') #This will add ducati to the list
+print(bicycles) #This will print the list of bicycles
+print("\n") #This will print a new line
+#now lets add to the list
+bicycles.insert(0,'carrera') #This will add carrera to the list
+print(bicycles) #This will print the list of bicycles
+print("\n") #This will print a new line
+#now lets remove from the list
+del bicycles[0] #This will remove carrera from the list
+print(bicycles) #This will print the list of bicycles
+print("\n") #This will print a new line
+#now lets remove from the list
+del bicycles[1] #This will remove cannondale from the list
+print(bicycles) #This will print the list of bicycles
+print("\n") #This will print a new line
+#now lets remove from the list
+del bicycles[2] #This will remove specialized from the list
+print(bicycles) #This will print the list of bicycles
+print("\n") #This will print a new line
+# lets add a list to the list of bicycles
+motorcycles = ['honda','yamaha','suzuki'] #This is a list of motorcycles
+print(motorcycles) #This will print the list of motorcycles
+print("\n") #This will print a new line
+# lets add a list to the list of bicycles
+newlist = bicycles + motorcycles #This will add the list of bicycles to the list of motorcycles
+print(newlist) #This will print the new list
